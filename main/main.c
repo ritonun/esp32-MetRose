9eca5bd36845efbc41e6a2f882d0f314cc4c4f11
ritonun <73615644+ritonun@users.noreply.github.com>
2025-10-12 19:50:35 +0200

    add wifi (simplest connect)

--- main/main.c
+++ main/main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+//main.c
+#include "nvs_flash.h"
+#include "esp_log.h"
+#include "wifi.h"
+
+static const char* TAG = "main";
 
 void app_main(void)
 {
+    // Init non-volatile storage (NVS)
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
+    ESP_LOGI(TAG, "Starting Wi-Fi Station example...");
+    wifi_init_sta();  // from wifi.c
 }
---
+++ main/wifi.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "esp_err.h"
+
+void wifi_init_sta(void);
