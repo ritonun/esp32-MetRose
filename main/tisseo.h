2f62b3d4013841037a87cddf367d7402e0e48e71
ritonun <73615644+ritonun@users.noreply.github.com>
2025-10-13 14:14:03 +0200

    remove prototype fn in tisseo.h

--- main/tisseo.h
+++ main/tisseo.h
@@ -4,7 +4,5 @@
 #include "esp_err.h"
 #include "cJSON.h"
 
-esp_err_t tisseo_init(const char *API_KEY);
-void *tisseo_request(const char *service, const char *format, const char *params_query);
 cJSON *tisseo_get_stops_schedules(const char *stop_area_id, const char *line_id);
-void tisseo_deinit(void);
+
